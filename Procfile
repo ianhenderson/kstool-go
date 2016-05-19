e743e62b6466e5ca4d8f9313ef42249a567b8159
Ian Henderson <ianwhen@gmail.com>
2016-05-19 16:27:27 -0700

    Move server.go to src/ - Add vendor dependencies - Add Procfile, format code

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: kstool-go
--- main.go
+++ main.go
@@ -4,20 +4,26 @@ import (
 	// Internal libs
 	"fmt"
 	"net/http"
+	"os"
 	// External libs
 	"github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    output := "Hi! You've requested: /" + vars["pageId"]
+	vars := mux.Vars(r)
+	output := "Hi! You've requested: /" + vars["pageId"]
 	fmt.Println(output)
 	fmt.Fprintf(w, output)
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", handler)
 	router.HandleFunc("/{pageId}", handler)
-	http.ListenAndServe(":8080", router)
+	fmt.Println("Listening on port", port)
+	http.ListenAndServe(":"+port, router)
 }

943f4eacf12c0c2110f4e47781d28cf7d1fb2573
Ian Henderson <ianwhen@gmail.com>
2016-05-20 14:49:41 -0700

    Update default port

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		port = "8080"
+		port = "8000"
 	}
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", handler)
